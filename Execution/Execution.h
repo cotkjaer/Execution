b1908d46ef50fcc958a63d7ae79820c0612ede1f
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 16:04:45 +0100

    Initial Commit

---
+++ Execution/Execution.h
@@ -0,0 +1,19 @@
+//
+//  Execution.h
+//  Execution
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Execution.
+FOUNDATION_EXPORT double ExecutionVersionNumber;
+
+//! Project version string for Execution.
+FOUNDATION_EXPORT const unsigned char ExecutionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Execution/PublicHeader.h>
+
+
