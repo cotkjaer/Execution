ab3680ebfcab40c3210d657a37105f8ddf555a8a
christian otkjær <christian.otkjaer@gmail.com>
2016-10-13 09:38:51 +0200

    iOS 10

--- Execution/Async.swift
+++ Execution/Async.swift
@@ -51,7 +51,31 @@ public extension DispatchTime
 }
 */
 
-// MARK: - <#comment#>
+public func delay(bySeconds seconds: Double, dispatchLevel: DispatchLevel = .main, closure: @escaping () -> ())
+{
+    let dispatchTime = DispatchTime.now() + Double(Int64(seconds * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+    
+    dispatchLevel.dispatchQueue.asyncAfter(deadline: dispatchTime, execute: closure)
+}
+
+public enum DispatchLevel
+{
+    case main, userInteractive, userInitiated, utility, background
+    
+    var dispatchQueue: DispatchQueue
+    {
+        switch self
+        {
+        case .main:             return DispatchQueue.main
+        case .userInteractive:  return DispatchQueue.global(qos: .userInteractive)
+        case .userInitiated:    return DispatchQueue.global(qos: .userInitiated)
+        case .utility:          return DispatchQueue.global(qos: .utility)
+        case .background:       return DispatchQueue.global(qos: .background)
+        }
+    }
+}
+
+// MARK: - After
 
 extension DispatchQueue
 {
